e6abed6c7578710a750769ede40e33ab57b320e2
Letenyei Ádám <letenyeidam89@gmail.com>
2021-03-21 18:26:44 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,6 +7,8 @@ function flip(id) {
     let box = document.getElementById(id);
     
     box.classList.add("flip-vertical-left");
+    box.style.backgroundImage = "none";
+    box.style.background = "white";
     //box.style.backgroundImage = "url('pic/first.png')";
     
     //document.getElementById(id).classList.add("flip-vertical-left");
