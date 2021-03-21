cdd0b0763ee21e164eefc63bf1823d76f49a8587
Letenyei Ádám <letenyeidam89@gmail.com>
2021-03-21 18:23:23 +0100

    Css js
    cards background

--- script/script.js
+++ script/script.js
@@ -2,3 +2,12 @@ function switchdiv(id) {
     console.log("id: " + id);
     
 }
+
+function flip(id) {
+    let box = document.getElementById(id);
+    
+    box.classList.add("flip-vertical-left");
+    //box.style.backgroundImage = "url('pic/first.png')";
+    
+    //document.getElementById(id).classList.add("flip-vertical-left");
+}
