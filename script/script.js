12b19052a81e336338021279207fd7e0a9319538
Letenyei Ádám <letenyeidam89@gmail.com>
2021-03-28 16:44:50 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,14 +2,20 @@
 function switchdiv(id) {
     
     if(id === "homeBtn") {
+
         $("#bookCont").hide(1000);
         $("#gamesCont").hide(1000);
+
     } else if(id === "bookBtn") {
+
         $("#gamesCont").hide(1000);
         $("#bookCont").toggle(1000);
+
     } else {
-       $("#bookCont").hide(1000);
+
+        $("#bookCont").hide(1000);
         $("#gamesCont").toggle(1000); 
+
     }   
 }
 
@@ -23,12 +29,14 @@ $("#first").click(function(){
         selected.removeClass("flip-vertical-left");
         selected.css("background","url('pic/first.png')");
         selected.css("backgroundSize","cover");
+
     } else {
 
         hideAuthorInfo();
         selected.addClass("flip-vertical-left");
         selected.css("background","url('pic/first-bg.png')");
         selected.css("backgroundSize","cover");
+        $("#first_book_description").show();
     }
 });
 
@@ -50,6 +58,7 @@ $("#second").click(function() {
         selected.addClass("flip-vertical-left");
         selected.css("background","url('pic/second-bg.png')");
         selected.css("backgroundSize","cover");
+
     }
 });
 
@@ -63,12 +72,14 @@ $("#third").click(function() {
         selected.removeClass("flip-vertical-left");
         selected.css("background","url('pic/third.png')");
         selected.css("backgroundSize","cover");
+
     } else {
 
         hideAuthorInfo();
         selected.addClass("flip-vertical-left");
         selected.css("background","url('pic/third-bg.png')");
         selected.css("backgroundSize","cover");
+
     }
 });
 
@@ -78,23 +89,3 @@ let hideAuthorInfo = function() {
 let showAuthorInfo = function() {
     $(".authorInfo").show();
 }
-
-$( function() {
-    var state = true;
-    $( "#button" ).on( "click", function() {
-      if ( state ) {
-        $( "#effect" ).animate({
-          backgroundColor: "#aa0000",
-          color: "#fff",
-          width: 500
-        }, 1000 );
-      } else {
-        $( "#effect" ).animate({
-          backgroundColor: "#fff",
-          color: "#000",
-          width: 240
-        }, 1000 );
-      }
-      state = !state;
-    });
-  } );
